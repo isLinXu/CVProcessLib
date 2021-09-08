8fa160139fc3f7ce0b120f60f9227154dd1a53fa
魏明泉 <1830465230@qq.com>
2021-09-08 16:53:24 +0800

    b02 push v1.4 增加ABB-SACE-Emax-X1款式定位

--- core/cv/arucoMarkerCode/dlq4b02.py
+++ core/cv/arucoMarkerCode/dlq4b02.py
@@ -17,7 +17,7 @@ class Find(object):
             if not os.path.exists(self.save_path):
                 os.mkdir(self.save_path)
 
-    def dlq_location(self, img, ks=None, save_name=None):
+    def dlq_location(self, img, ks=None, save_name=None, dlq_type=None):
 
         if ks is not None:
             img_ = cv2.GaussianBlur(img, ks, 0)
@@ -41,10 +41,19 @@ class Find(object):
             x_max = max(point1[0], max(point2[0], max(point3[0], point4[0])))
             y_max = max(point1[1], max(point2[1], max(point3[1], point4[1])))
 
-            lower = y_min
-            upper = y_min - (y_max - y_min) * 1.1
-            left = x_min - (x_max - x_min) * 1.5
-            right = x_max + (x_max - x_min) * 1.5
+            if dlq_type == 'ABB-SACE-Emax':
+                lower = y_min
+                upper = y_min - (y_max - y_min) * 1.1
+                left = x_min - (x_max - x_min) * 1.5
+                right = x_max + (x_max - x_min) * 1.5
+
+            elif dlq_type == 'ABB-SACE-Emax-X1':
+                lower = y_max + (y_max - y_min) * 0.8
+                upper = y_max
+                left = x_min - (x_max - x_min) * 1.5
+                right = x_max + (x_max - x_min) * 1.5
+            else:
+                return None
 
             # lower = point1[1]
             # upper = point1[1] - (point2[1] - point1[1]) * 1.1
@@ -69,13 +78,14 @@ if __name__ == '__main__':
 
     import os
 
-    images_path = r'/home/hxzh/Wei_Work/Project/Dataset/2021-09-06/'
+    images_path = r'/home/hxzh/Wei_Work/Project/Dataset/0908/'
+    dlq_type = 'ABB-SACE-Emax-X1'
     images = os.listdir(images_path)
     DFind = Find(is_save=True, save_path='./crop/')
     for i, image_name in enumerate(images):
         image = cv2.imread(os.path.join(images_path, image_name))
         # image = cv2.resize(image, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC)
-        crop_img = DFind.dlq_location(image, (31, 31), image_name)
+        crop_img = DFind.dlq_location(image, (31, 31), image_name, dlq_type)
         if crop_img is not None:
             print(i+1)
             cv2.imshow('temp', crop_img)
