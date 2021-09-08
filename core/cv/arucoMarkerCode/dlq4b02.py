6432d1d0e9cb5a7b470186df2a3831f13276bfdb
魏明泉 <1830465230@qq.com>
2021-09-08 09:53:11 +0800

    b02 push v1.3

--- core/cv/arucoMarkerCode/dlq4b02.py
+++ core/cv/arucoMarkerCode/dlq4b02.py
@@ -1,22 +1,29 @@
-import time
-
-import numpy as np
 import cv2
 import cv2.aruco as aruco
 
 
 class Find(object):
 
-    def __init__(self):
+    def __init__(self, is_save=False, save_path=None):
         super(Find, self).__init__()
+
+        self.is_save = is_save
+        self.save_path = save_path
 
         self.aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_250)
         self.parameters = aruco.DetectorParameters_create()
 
-    def dlq_location(self, img, ks=(11, 11)):
+        if self.is_save:
+            if not os.path.exists(self.save_path):
+                os.mkdir(self.save_path)
 
-        img_ = cv2.GaussianBlur(img, ks, 0)
-        gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+    def dlq_location(self, img, ks=None, save_name=None):
+
+        if ks is not None:
+            img_ = cv2.GaussianBlur(img, ks, 0)
+            gray = cv2.cvtColor(img_, cv2.COLOR_BGR2GRAY)
+        else:
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         # gray = cv2.resize(gray, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC)
         # start_func_time = time.time()
         corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, self.aruco_dict, parameters=self.parameters)
@@ -47,6 +54,9 @@ class Find(object):
             out = img[int(upper):int(lower), int(left):int(right)]
 
             if out.shape[0] > 0 and out.shape[1] > 0:
+                if self.save_path and self.is_save and save_name:
+                    image_save_path = self.save_path + save_name
+                    cv2.imwrite(image_save_path, out)
                 return out
             else:
                 return None
@@ -61,12 +71,15 @@ if __name__ == '__main__':
 
     images_path = r'/home/hxzh/Wei_Work/Project/Dataset/2021-09-06/'
     images = os.listdir(images_path)
-    DFind = Find()
-    for image_name in images:
+    DFind = Find(is_save=True, save_path='./crop/')
+    for i, image_name in enumerate(images):
         image = cv2.imread(os.path.join(images_path, image_name))
         # image = cv2.resize(image, None, fx=0.4, fy=0.4, interpolation=cv2.INTER_CUBIC)
-        crop_img = DFind.dlq_location(image, (3, 3))
+        crop_img = DFind.dlq_location(image, (31, 31), image_name)
         if crop_img is not None:
+            print(i+1)
             cv2.imshow('temp', crop_img)
             cv2.waitKey(500)
+        else:
+            print(image_name)
 
