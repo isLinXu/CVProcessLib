63aaa7267db65ec52d43f4a28f8ee1e59b083bfa
isLinXu <2267379130@qq.com>
2022-06-30 18:11:36 +0800

    1.update selectROI.py

--- core/cv/ROI/selectROI.py
+++ core/cv/ROI/selectROI.py
@@ -7,18 +7,19 @@
 import cv2
 import imutils
 
-img = cv2.imread("/home/linxu/PycharmProjects/CVProcessLib/images/状态指示器/2.jpeg")
-img = imutils.resize(img, width=500)
+img = cv2.imread("/home/linxu/Desktop/checkmarkdetection/data/2022_03_30T11_46_12_719318_1.jpg")
+# img = imutils.resize(img, width=640)
 
 roi = cv2.selectROI(windowName="selectROI", img=img, showCrosshair=True, fromCenter=False)
 x, y, w, h = roi
 
-cv2.rectangle(img=img, pt1=(x, y), pt2=(x + w, y + h), color=(0, 0, 255), thickness=1)
-cv2.imshow("selectROI", img)
+# cv2.rectangle(img=img, pt1=(x, y), pt2=(x + w, y + h), color=(0, 0, 255), thickness=1)
+# cv2.imshow("selectROI", img)
 
 print('ROI:', roi)
 ROI = img[y:y+h, x:x+w]
 cv2.imshow('ROI',ROI)
+cv2.imwrite('dst.jpg', ROI)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
