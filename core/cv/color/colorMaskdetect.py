7c69f6248109dda926d80f13ea029ac0772978b3
Xu Lin <2267379130@qq.com>
2022-11-10 16:01:17 +0800

    1.update color_match.py 2.update colorMaskdetect.py

--- core/cv/color/colorMaskdetect.py
+++ core/cv/color/colorMaskdetect.py
@@ -30,8 +30,8 @@ def overlay_mask(mask, image):
 
 def find_biggest_contour(image):
     image = image.copy()
-    contours, hierarchy = cv2.findContours(image, cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
-    contour_sizes = [(cv2.contourArea(contour), contour) for contour in    contours]
+    contours, hierarchy = cv2.findContours(image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contour_sizes = [(cv2.contourArea(contour), contour) for contour in contours]
     biggest_contour = max(contour_sizes, key=lambda x: x[0])[1]
     mask = np.zeros(image.shape, np.uint8)
     cv2.drawContours(mask, [biggest_contour], -1, 255, -1)
@@ -42,13 +42,13 @@ def find_biggest_contour(image):
 def circle_countour(image, countour):
     image_with_ellipse = image.copy()
     ellipse = cv2.fitEllipse(countour)
-    cv2.ellipse(image_with_ellipse, ellipse, (0,255,0), 2)
+    cv2.ellipse(image_with_ellipse, ellipse, (0, 255, 0), 2)
 
     return image_with_ellipse
 
 
 # First load the image and examine the properties of that image, such as the color spectrum and the dimensions.
-image = cv2.imread('/home/linxu/PycharmProjects/CVProcessLib/images/lena.png')
+image = cv2.imread('/home/linxu/Desktop/南宁电厂项目/OCR/0_0_0_15_0_0/1000001_20220812230113_v.jpeg')
 
 # Since the order of the image stored in the memory is Blue Green Red (BGR), we need to convert it into RGB.
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -56,7 +56,7 @@ image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 # We will be scaling down the image dimensions.
 max_dimension = max(image.shape)
 scale = 700 / max_dimension
-image = cv2.resize(image, None, fx=scale,fy=scale)
+image = cv2.resize(image, None, fx=scale, fy=scale)
 
 # Gaussian filters are very popular in the research field and are used for various operations,
 # one of which is the blurring effect that reduces the noise and balances the image.
@@ -66,17 +66,17 @@ image_blur = cv2.GaussianBlur(image, (7, 7), 0)
 # to extract other characteristics of the image using color intensity, brightness, and shades.
 image_blur_hsv = cv2.cvtColor(image_blur, cv2.COLOR_RGB2HSV)
 
-red = np.uint8([[[255,28, 28]]])
+red = np.uint8([[[255, 28, 28]]])
 hsv_red = cv2.cvtColor(red, cv2.COLOR_BGR2HSV)
 
 # We need to create a mask that can detect the specific color RED, in our case.
 min_red = np.array([0, 100, 100])
 max_red = np.array([10, 255, 255])
-mask1 = cv2.inRange(image_blur_hsv, min_red, max_red) # Filter by color
+mask1 = cv2.inRange(image_blur_hsv, min_red, max_red)  # Filter by color
 min_red = np.array([170, 100, 80])
 max_red = np.array([180, 255, 255])
-mask2 = cv2.inRange(image_blur_hsv, min_red, max_red) # Filter by brightness
-mask = mask1 + mask2 # Concatenate both the mask for better feature extraction
+mask2 = cv2.inRange(image_blur_hsv, min_red, max_red)  # Filter by brightness
+mask = mask1 + mask2  # Concatenate both the mask for better feature extraction
 
 # Once we are able to create our mask successfully, we need to perform some morphological
 # operations, which are basic image processing operations used for the analysis and
@@ -90,9 +90,9 @@ mask_closed = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 mask_clean = cv2.morphologyEx(mask_closed, cv2.MORPH_OPEN, kernel)
 
 # It's time to use the mask that we created to extract the object from the image.
-big_contour, red_mask = find_biggest_contour(mask_clean) # Extract biggest bounding box
-overlay = overlay_mask(red_mask, image) # Apply mask
-circled = circle_countour(overlay, big_contour) # Draw bounding box
+big_contour, red_mask = find_biggest_contour(mask_clean)  # Extract biggest bounding box
+overlay = overlay_mask(red_mask, image)  # Apply mask
+circled = circle_countour(overlay, big_contour)  # Draw bounding box
 
 # Voila! So, we successfully extracted the image and also drew the bounding box around the object
 show(circled)
--- core/cv/color/color_match.py
+++ core/cv/color/color_match.py
@@ -4,7 +4,54 @@ import numpy as np
 
 
 # 根据颜色提取对应色域
-def getColorMask(img):
+def get_find_Color(img, lower = (0,0,0), higher= (255,255,255)):
+    lowHue = lower[0]
+    lowSat = lower[1]
+    lowVal = lower[2]
+    highHue = higher[0]
+    highSat = higher[1]
+    highVal = higher[2]
+
+    # 显示原始图像。
+    cv2.imshow('frame', img)
+
+    # 可选择不同的模糊方法
+    frameBGR = cv2.GaussianBlur(img, (7, 7), 0)
+    # frameBGR = cv2.medianBlur(frameBGR, 7)
+    # frameBGR = cv2.bilateralFilter(frameBGR, 15 ,75, 75)
+    # kernal = np.ones((15, 15), np.float32)/255
+    # frameBGR = cv2.filter2D(frameBGR, -1, kernal)
+    # cv2.imshow('frameBGR_kernal', frameBGR)
+
+    # 显示模糊图像。
+    cv2.imshow('blurred', frameBGR)
+    # 转换为HSV颜色空间
+    hsv = cv2.cvtColor(frameBGR, cv2.COLOR_BGR2HSV)
+
+    # 定义HSV值颜色范围
+    colorLow = np.array([lowHue, lowSat, lowVal])
+    colorHigh = np.array([highHue, highSat, highVal])
+    mask = cv2.inRange(hsv, colorLow, colorHigh)
+    # 显示mask
+    cv2.imshow('mask-plain', mask)
+
+    kernal = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernal)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernal)
+
+    # 显示形态变换遮罩
+    cv2.imshow('mask', mask)
+
+    # 将遮罩放在原始图像的上方。
+    result = cv2.bitwise_and(img, img, mask=mask)
+
+    # 显示最终输出图像
+    cv2.imshow('colorTest', result)
+    cv2.waitKey()
+    return mask
+
+# 根据颜色提取对应色域
+def getColorMask(img, lower = (0,0,0), higher= (255,255,255)):
     '''
     根据颜色提取对应色域
     :param img:
@@ -14,18 +61,41 @@ def getColorMask(img):
     '''
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     # 掩模提取色域
-    lower = (0, 0 ,1)
-    higher = (0,255,255)
     mask = cv2.inRange(img_hsv, lower, higher)
     # mask = cv2.medianBlur(mask, 3)
+    # cv2.imshow('mask-plain', mask)
+
+    # kernal = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+    # mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernal)
+    # mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernal)
+
+    # 显示形态变换遮罩
+    # cv2.imshow('mask', mask)
+
     return mask
 
 if __name__ == '__main__':
-    img_path = '/home/hxzh02/文档/plane-project_beijing/lib4mod/linebreak/output/16_4050.png'
+    # img_path = '/home/linxu/Desktop/南宁电厂项目/OCR/0_0_0_15_0_0/1000001_20220812230113_v.jpeg'
+    img_path = '/home/linxu/Desktop/南宁电厂项目/OCR/0_0_0_15_0_0/1000034_20220813222452_v.jpeg'
     src = cv2.imread(img_path)
     cv2.imshow('src', src)
 
-    mask_red = getColorMask(src)
+    lower = (0, 70, 152)
+    higher = (255, 255, 255)
+
+    # lower = (0, 70, 140)
+    # higher = (255, 140, 255)
+
+    # lower = (0,40,117)
+    # higher = (255,196,255)
+
+    # lower = (0, 76, 135)
+    # higher = (255, 195, 255)
+
+    # lower = (0, 0 ,1)
+    # higher = (0,255,255)
+
+
+    mask_red = getColorMask(src, lower, higher)
     cv2.imshow('mask_red', mask_red)
-    cv2.waitKey()
     cv2.waitKey()
