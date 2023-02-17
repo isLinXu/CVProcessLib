682a718012d6e2cb15950503fff57b7175d06fc5
isLinXu <2267379130@qq.com>
2023-02-17 16:35:01 +0800

    update selectROI.py

--- core/cv/ROI/selectROI.py
+++ core/cv/ROI/selectROI.py
@@ -7,20 +7,26 @@
 import cv2
 import imutils
 
-img = cv2.imread("/home/linxu/Desktop/checkmarkdetection/data/2022_03_30T11_46_12_719318_1.jpg")
-# img = imutils.resize(img, width=640)
+def select_roi_image(img):
+    roi = cv2.selectROI(windowName="selectROI", img=img, showCrosshair=True, fromCenter=False)
+    x, y, w, h = roi
 
-roi = cv2.selectROI(windowName="selectROI", img=img, showCrosshair=True, fromCenter=False)
-x, y, w, h = roi
+    cv2.rectangle(img=img, pt1=(x, y), pt2=(x + w, y + h), color=(0, 0, 255), thickness=1)
+    cv2.imshow("selectROI", img)
+    print('ROI:', roi)
+    ROI = img[y:y+h, x:x+w]
+    cv2.imshow('ROI',ROI)
+    cv2.imwrite('dst.jpg', ROI)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-# cv2.rectangle(img=img, pt1=(x, y), pt2=(x + w, y + h), color=(0, 0, 255), thickness=1)
-# cv2.imshow("selectROI", img)
 
-print('ROI:', roi)
-ROI = img[y:y+h, x:x+w]
-cv2.imshow('ROI',ROI)
-cv2.imwrite('dst.jpg', ROI)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    file_path = '/home/linxu/Desktop/ORC/数据需求-230216/1、功能1 数据源_defect map/1  4N46-NX03216 ALL.png'
+    img = cv2.imread(file_path)
+    # img = imutils.resize(img, width=640)
+    select_roi_image(img)
+
+
 
 
